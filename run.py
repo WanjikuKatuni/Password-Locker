9f19d9cab9bb09ba4f586b4a4194d01190d5cd1d
wanjiku <annritawanjiku77@gmail.com>
2022-04-26 05:05:44 +0300

    add welcome message

--- run.py
+++ run.py
@@ -3,7 +3,17 @@ from credentials import Credentials
 
 
 def main():
+
+    print("Welcome to PasswordLocker. What should we call you?")
+
+    name=input()
+
+    print(f"Hello {name}. To proceed selecect the shortcodes below")
+    print('/n')
+
     
+
+        
 
 """ 
     to ensure the main file is executed
