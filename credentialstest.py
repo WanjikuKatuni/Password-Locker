9ac948890559f0ae41901d833ab2a0e57d55d065
wanjiku <annritawanjiku77@gmail.com>
2022-04-26 09:37:48 +0300

    add tear down to clear test cases

--- credentialstest.py
+++ credentialstest.py
@@ -29,8 +29,14 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.account_password,"123")
 
     def test_save_credentials(self):
+        """ check if save credentials saves and gives the result """
+
         self.new_credentials.save_credentials()
         self.assertEqual(len(Credentials.credentials_list),1)
+    
+    def tearDown(self):
+        """ to clear the test cases so as to avoid len errors"""
+        Credentials.credentials_list=[]
 
 
 if __name__== '__main__':
