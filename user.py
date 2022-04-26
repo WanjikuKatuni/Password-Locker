5c407f273934b5e64a02a5d12448fa27dda4dce5
wanjiku <annritawanjiku77@gmail.com>
2022-04-26 04:36:44 +0300

    define save user to save objects in the user list

--- user.py
+++ user.py
@@ -8,3 +8,9 @@ class User:
     def __init__(self,user_name, password):
         self.user_name = user_name
         self.password =  password
+
+    def save_user(self):
+        """
+        save_user to save new objects into the user list
+        """
+        User.user_list.append(self)
