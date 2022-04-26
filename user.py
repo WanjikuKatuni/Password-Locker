1c7dbe5bd93726dacfbe1f58beb3d20370473981
wanjiku <annritawanjiku77@gmail.com>
2022-04-26 04:34:10 +0300

    add instances of the user class

--- user.py
+++ user.py
@@ -2,4 +2,9 @@ class User:
     """ 
     this stores the user password locker login credentials
     """
-    
+
+    user_list = [] #empty user list
+
+    def __init__(self,user_name, password):
+        self.user_name = user_name
+        self.password =  password
