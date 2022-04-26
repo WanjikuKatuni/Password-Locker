5c41884c18c67662d206d2389bc67bca00227cbd
wanjiku <annritawanjiku77@gmail.com>
2022-04-26 09:30:08 +0300

    check working new_credentials

--- credentialstest.py
+++ credentialstest.py
@@ -12,5 +12,22 @@ class TestCredentials(unittest.TestCase):
     args:
     unittest.Testcase - Testcase class assists in creating test cases
     """
+    def setUp(self):
+        """
+        used to define methods which will be executed by the test method
+        """
+        self.new_credentials = Credentials("instagram","ann","123")
 
-    
+
+    def test_init(self):
+        """
+        define test method
+        """
+
+        self.assertEqual(self.new_credentials.account_type,"instagram")
+        self.assertEqual(self.new_credentials.account_username,"ann")
+        self.assertEqual(self.new_credentials.account_password,"123")
+
+
+if __name__== '__main__':
+    unittest.main()
