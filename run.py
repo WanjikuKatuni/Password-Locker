d0994d39871870b00840ef2e3d9bf311d6d4d178
wanjiku <annritawanjiku77@gmail.com>
2022-04-26 06:18:39 +0300

    add login of guest user with preset logins

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def main():
     """ while loop to achieve application requisites """
 
     while True:
-        print("To create new username use 'new':'/n' To login to existing account use 'log': '/n' T0 exit the applicaiton use 'x'")
+        print("To create new username use 'new':'/n' To login to guest account use 'log': '/n' T0 exit the applicaiton use 'x'")
         short_code = input().lower() #ensure shortcodes are in lowercase
         print('/n')
 
@@ -39,7 +39,7 @@ def main():
                 print('/n')
             
             else:
-                print(f"Congratulations!!! Account creation successful Welcome {user_name} to your password locker VAULT!!")
+                print(f"Congratulations!!! Account creation successful Welcome {new_user_name} to your password locker VAULT!!")
                 print('/n')
                 print("Enter Login details")
                 print("Username")
@@ -47,14 +47,41 @@ def main():
                 print("Password")
                 existing_password=input()
             
-            while existing_user_name != new_user_name or existing_password != new_password:
-                print("Oopsie!! Memory can be fleeting at times :) Invalid username or password")
-                print('/n')
-                print("Enter Login details again.This time, get it right")
-                print("Username")
-                existing_user_name = input()
-                print("Password")
-                existing_password=input()
+                while existing_user_name != new_user_name or existing_password != new_password:
+                    print("Oopsie!! Memory can be fleeting at times :) Invalid username or password")
+                    print('/n')
+                    print("Enter Login details again.This time, get it right")
+                    print("Username")
+                    existing_user_name = input()
+                    print("Password")
+                    existing_password=input()
+                else:
+                    print(f"Welcome {existing_user_name} to your password vault account")
+                    print('/n')
+            
+        elif short_code == 'log':
+            print('Welcome to your Vault')
+            print("Enter Login details")
+            print("Username")
+            preset_user_name = input()
+            print("Password")
+            preset_password = input()
+
+            while preset_user_name != 'guestuser' or existing_password != 12345:
+                    print("Oopsie!! For guest login use the username 'guestuser' and password '12345' ")
+                    print('/n')
+                    print("Enter Login details again.This time, get it right")
+                    print("Username")
+                    preset_user_name = input()
+                    print("Password")
+                    preset_password=input()
+            else:
+                    print(f"Welcome {preset_user_name} to your guest password vault account")
+                    print('/n')
+
+
+  
+        
             
 
 
